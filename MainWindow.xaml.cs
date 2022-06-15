7fb7302397e83c466fa171cd401d114ab0e2017b
Quantum0 <quantum0@mail.ru>
2022-06-15 15:22:21 +0300

    Update form with more buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -65,11 +65,9 @@ namespace SAR_Overlay
 
         private void ButtonMatchID_Click(object sender, RoutedEventArgs e) => SAR.ChatInput("/matchid");
 
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            // Wolfram: atan(pi-x/30) / pi + 0.6; x from 0 to 500; y from 0 to 1
+        // Wolfram: atan(pi-x/30) / pi + 0.6; x from 0 to 500; y from 0 to 1
+        private void dispatcherTimer_Tick(object sender, EventArgs e) =>
             this.Opacity = Math.Atan(Math.PI - Math.Max(1, this.Top - NativeMethods.GetMousePosition().Y) / 30) / Math.PI + 0.6;
-        }
 
         private void ButtonNight_Click(object sender, RoutedEventArgs e) => SAR.SwitchNight();
 
@@ -77,10 +75,7 @@ namespace SAR_Overlay
 
         private void ButtonSoccer_Click(object sender, RoutedEventArgs e) => SAR.Soccer();
 
-        private void ButtonTeleport_Click(object sender, RoutedEventArgs e)
-        {
-            new FormTeleport(SAR).ShowDialog();
-        }
+        private void ButtonTeleport_Click(object sender, RoutedEventArgs e) => new FormTeleport(SAR).ShowDialog();
 
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
@@ -110,5 +105,26 @@ namespace SAR_Overlay
             SAR.GasDamage = (float)SliderGasDamage.Value;
             SliderGasDamage.Value = SAR.GasDamage;
         }
+
+        private void ButtonSpawn_Click(object sender, RoutedEventArgs e)
+        {
+            // Open form to select item
+        }
+
+        private void ButtonOneHits_Click(object sender, RoutedEventArgs e) => SAR.OneHits = !SAR.OneHits;
+
+        private void ButtonFlight_Click(object sender, RoutedEventArgs e) => SAR.Flight();
+
+        private void ButtonKill_Click(object sender, RoutedEventArgs e) => new FormSelectPlayer(SAR, SAR.Kill).ShowDialog();
+
+        private void ButtonKick_Click(object sender, RoutedEventArgs e) => new FormSelectPlayer(SAR, SAR.Kick).ShowDialog();
+
+        private void ButtonInfect_Click(object sender, RoutedEventArgs e) => new FormSelectPlayer(SAR, SAR.Infect).ShowDialog();
+
+        private void ButtonGod_Click(object sender, RoutedEventArgs e) => new FormSelectPlayer(SAR, SAR.God).ShowDialog();
+
+        private void ButtonGhost_Click(object sender, RoutedEventArgs e) => new FormSelectPlayer(SAR, SAR.Ghost).ShowDialog();
+
+        private void ButtonGod_MouseRightButtonDown(object sender, MouseButtonEventArgs e) => SAR.God(SAR.Me);
     }
 }
