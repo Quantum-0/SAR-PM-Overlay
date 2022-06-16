c259e788c5d45a161502690f02c57431b3cc44e6
Quantum0 <quantum0@mail.ru>
2022-06-16 03:40:55 +0300

    Update Enums

--- SARFacade/InGameEntities/SAREnums.cs
+++ SARFacade/InGameEntities/SAREnums.cs
@@ -17,12 +17,35 @@ namespace SAR_Overlay.Enums
 
     public enum SARWeapon : int
     {
-        // TODO
+        Pistol = 0,
+        DualPistols = 1,
+        Magnum = 2,
+        Deagle = 3,
+        SilencedPistol = 4,
+        Shotgun = 5,
+        JAG7 = 6,
+        SMG = 7,
+        ThomasGun = 8,
+        AK = 9,
+        M16 = 10,
+        DognasDartGun = 11,
+        HuntingRifle = 12,
+        Sniper = 13,
+        Minigun = 14,
+        Bow = 15,
+        SparrowLauncher = 16,
+        BCG = 17,
     }
 
     public enum SARUtil : int
     {
-        // TODO
+        ClawBoots = 0,
+        BananaForker = 1,
+        NinjaBooties = 2,
+        SkunkGasSnorkel = 3,
+        Cupgrade = 4,
+        ImposibleTape = 5,
+        SuperBandoiler = 6,
     }
 
     public enum SARAmmo : int
